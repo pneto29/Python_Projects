5b7bdcb8cb3b7425e8ca207a59e9fd780d48a63a
no name <69532484+pneto29@users.noreply.github.com>
2023-02-12 15:29:32 -0300

    Add files via upload

---
+++ mnist_vvg16/main.py
@@ -0,0 +1,366 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import random
+from tqdm import tqdm  # for progress bar
+
+# Libraries for TensorFlow
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import models, layers
+from tensorflow import keras
+
+# Library for Transfer Learning
+from tensorflow.keras.applications import VGG16
+from keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.utils import array_to_img
+from tensorflow.keras.applications import VGG16
+
+print("Importing libraries completed.")
+
+# Loading dataset from keras
+
+(xtrain, ytrain), (xtest, ytest) = keras.datasets.mnist.load_data()
+
+# Verifying dataset
+
+print(xtrain.shape)
+print(ytrain.shape)
+print(xtest.shape)
+print(ytest.shape)
+print(ytrain)
+
+xtrain = np.dstack([xtrain] * 3)
+xtest = np.dstack([xtest] * 3)
+xtrain.shape, xtest.shape
+
+xtrain = xtrain.reshape(-1, 28, 28, 3)
+xtest = xtest.reshape(-1, 28, 28, 3)
+xtrain.shape, xtest.shape
+
+# Resize the images 48*48 as required by VGG16
+
+
+xtrain = np.asarray([img_to_array(array_to_img(im, scale=False).resize((48, 48))) for im in xtrain])
+xtest = np.asarray([img_to_array(array_to_img(im, scale=False).resize((48, 48))) for im in xtest])
+# train_x = preprocess_input(x)
+xtrain.shape, xtest.shape
+
+# # listing the folders containing images
+
+# preparing array that can be used later
+
+class_names = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+print(class_names)
+
+val_class_names = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+print(val_class_names)
+
+test_class_names = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+print(test_class_names)
+
+
+# Function to know the name of the element
+
+def Get_Element_Name(argument):
+    switcher = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+    }
+    return switcher.get(argument, "Invalid")
+
+
+print(Get_Element_Name(0))
+
+# Preparing data
+
+x = []  # to store array value of the images
+x = xtrain
+y = []  # to store the labels of the images
+y = ytrain
+
+test_images = []
+test_images = xtest
+test_images_Original = []
+test_images_Original = xtest
+test_image_label = []  # to store the labels of the images
+test_image_label = ytest
+
+val_images = []
+val_images = xtest
+val_images_Original = []
+val_images_Original = xtest
+val_image_label = []  # to store the labels of the images
+val_image_label = ytest  # to store the labels of the images
+
+print("Preparing Dataset Completed.")
+
+# Verifying the output
+
+# Training Dataset
+print("Training Dataset")
+
+x = np.array(x)  # Converting to np arrary to pass to the model
+print(x.shape)
+
+y = to_categorical(y)  # onehot encoding of the labels
+# print(y)
+print(y.shape)
+
+# Test Dataset
+print("Test Dataset")
+
+test_images = np.array(test_images)
+print(test_images.shape)
+
+test_image_label = to_categorical(test_image_label)  # onehot encoding of the labels)
+print(test_image_label.shape)
+
+# Validation Dataset
+print("Validation Dataset")
+
+val_images = np.array(val_images)
+print(val_images.shape)
+
+val_image_label = to_categorical(val_image_label)  # onehot encoding of the labels)
+print(val_image_label.shape)
+
+# Check properties of the model that we are going to use for Transfer Learning
+
+print("Summary of default VGG16 model.\n")
+
+# we are using VGG16 for transfer learnin here. So we have imported it
+
+# initializing model with weights='imagenet'i.e. we are carring its original weights
+model_vgg16 = VGG16(weights='imagenet')
+
+# display the summary to see the properties of the model
+model_vgg16.summary()
+# Modelling WITH Transfer Learning
+
+# Here we will prepare model as per our requirements
+
+print("Summary of Custom VGG16 model.\n")
+print("1) We setup input layer and 2) We removed top (last) layer. \n")
+
+# let us prepare our input_layer to pass our image size. default is (224,224,3). we will change it to (224,224,3)
+input_layer = layers.Input(shape=(48, 48, 3))
+
+# initialize the transfer model VGG16 with appropriate properties per our need.
+# we are passing paramers as following
+# 1) weights='imagenet' - Using this we are carring weights as of original weights.
+# 2) input_tensor to pass the VGG16 using input_tensor
+# 3) we want to change the last layer so we are not including top layer
+model_vgg16 = VGG16(weights='imagenet', input_tensor=input_layer, include_top=False)
+
+# See the summary of the model with our properties.
+model_vgg16.summary()
+
+# access the current last layer of the model and add flatten and dense after it
+
+print("Summary of Custom VGG16 model.\n")
+print("1) We flatten the last layer and added 1 Dense layer and 1 output layer.\n")
+
+last_layer = model_vgg16.output  # we are taking last layer of the model
+
+# Add flatten layer: we are extending Neural Network by adding flattn layer
+flatten = layers.Flatten()(last_layer)
+
+# Add dense layer
+dense1 = layers.Dense(100, activation='relu')(flatten)
+dense1 = layers.Dense(100, activation='relu')(flatten)
+dense1 = layers.Dense(100, activation='relu')(flatten)
+
+# Add dense layer to the final output layer
+output_layer = layers.Dense(10, activation='softmax')(flatten)
+
+# Creating modle with input and output layer
+model = models.Model(inputs=input_layer, outputs=output_layer)
+
+# Summarize the model
+model.summary()
+
+# we will freez all the layers except the last layer
+
+# we are making all the layers intrainable except the last layer
+print("We are making all the layers intrainable except the last layer. \n")
+for layer in model.layers[:-1]:
+    layer.trainable = False
+model.summary()
+
+# Train the Model
+
+from sklearn.model_selection import train_test_split
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=5)
+# print(xtrain)
+# print(xtest)
+# print(ytrain)
+# print(ytest)
+
+print("Splitting data for train and test completed.")
+
+# Compiling Model
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print("Model compilation completed.")
+model.summary()
+
+# Fit the Model
+
+# xtrain2=xtrain.reshape(60000,48,48,3)
+# xtest2=xtest.reshape(10000,48,48,3)
+
+history = model.fit(xtrain, ytrain, epochs=20, batch_size=128, verbose=True, validation_data=(xtest, ytest))
+
+print("Fitting the model completed.")
+
+
+# This function helps to predict individual image supplied to it
+
+# Function 1
+
+def predict(img_name):
+    img = image.load_img(img_name, target_size=(48, 48))
+    img = image.img_to_array(img)
+    plt.imshow(img.astype('int32'))
+    plt.show()
+    img = preprocess_input(img)
+
+    prediction = model.predict(img.reshape(1, 48, 48, 3))
+    output = np.argmax(prediction)
+
+    print(class_names[output] + ": " + Get_Element_Name(class_names[output]))
+
+
+# Function 2
+
+# This function plots the image supplied in array
+def plot_image(i, predictions_array, true_label,
+               img):  # taking index and 3 arrays viz. prediction array, true label array and image array
+
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img.astype('int32'))
+
+    predicted_label = np.argmax(predictions_array)
+    true_label = np.argmax(true_label)
+
+    if predicted_label == true_label:  # setting up label color
+        color = 'green'  # correct then blue colour
+    else:
+        color = 'red'  # wrong then red colour
+
+    plt.xlabel("{} {:2.0f}% \n ({})".format(Get_Element_Name(predicted_label),
+                                            100 * np.max(predictions_array), Get_Element_Name(true_label),
+                                            color=color, horizontalalignment='left'))
+
+
+#     plt.xlabel("{} {:2.0f}% ({})".format(val_class_names[predicted_label],
+#                                          100*np.max(predictions_array), val_class_names[true_label]),
+#                                          color=color)
+
+
+# Function 3
+
+# This function plots bar chart supplied in the array data
+def plot_value_array(i, predictions_array, true_label):  # taking index along with predictions and true label array
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    predicted_label = np.argmax(predictions_array)
+    true_label = np.argmax(true_label)
+
+    if predicted_label == 0:
+        predicted_label = 1
+    if true_label == 0:
+        true_label = 1
+
+    thisplot = plt.bar(range(10), predicted_label, color='seashell')
+    plt.ylim([0, 1])
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('green')
+
+
+# Preparing prediction arrary
+predictions = []
+
+for img in tqdm(val_images):
+    img = img.reshape(1, 48, 48, 3)
+    predictions.append(model.predict(img))
+
+# Prediction of individual images taken from internet
+
+# call the function
+
+# defining parameters to pass to function
+i = random.randrange(1, 10000)  # image number 12. You may change value of i for play around
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+# we are passing "val_images_Original" just to show original image instead of "val_images"
+# which is preprocessed as VGG16 process and used for prediction.
+plot_image(i, predictions, val_image_label, val_images_Original)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions, val_image_label)
+plt.show()
+
+# Declaring variables
+num_rows = 5
+num_cols = 5
+num_images = num_rows * num_cols
+
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+
+print("Classification of using Transfer Learning (VGG16)\n")
+print("Predicted, Percentage, (Original)\n")
+
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    ii = random.randrange(1, 10000)
+    # we are passing "val_images_Original" just to show original image instead of "val_images"
+    # which is preprocessed as VGG16 process and used for prediction.
+    plot_image(ii, predictions, val_image_label, val_images_Original)
+
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+
+    plot_value_array(i, predictions, val_image_label)
+plt.subplots_adjust(hspace=0.5)
+plt.show()
+
+# plot the loss and accuracy
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.title('Training and validation accuracy')
+plt.plot(epochs, acc, 'red', label='Training acc')
+plt.plot(epochs, val_acc, 'blue', label='Validation acc')
+plt.legend()
+
+plt.figure()
+plt.title('Training and validation loss')
+plt.plot(epochs, loss, 'red', label='Training loss')
+plt.plot(epochs, val_loss, 'blue', label='Validation loss')
+
+plt.legend()
+
+plt.show()
